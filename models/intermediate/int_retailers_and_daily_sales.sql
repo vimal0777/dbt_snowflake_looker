01557261448c1c6bea025914158ec4c914408090
Data Saavy <datasaavy2023@gmail.com>
2023-09-13 14:58:51 +0000

    intermediate

--- models/intermediate/int_retailers_and_daily_sales.sql
+++ models/intermediate/int_retailers_and_daily_sales.sql
@@ -1,28 +1,53 @@
-WITH retailers AS (
-    SELECT RETAILER_CODE,
-           RETAILER_NAME AS COMPANY_NAME,
-           TYPE AS COMPANY_TYPE,
-           COUNTRY
-    FROM {{ ref('stg_retailers') }}
-),
+with
+    retailers as (
+        select
+            retailer_code, retailer_name as company_name, type as company_type, country
+        from {{ ref("stg_retailers") }}
+    ),
 
-daily_sales AS (
-    SELECT RETAILER_CODE,
-           PRODUCT_NUMBER,
-           ORDER_METHOD_CODE,
-           Date_ AS ORDERED_DATE,
-           QUANTITY AS QUANTITY_SOLD,
-           SELLING_PRICE,
-           COST_PRICE
+    daily_sales as (
+        select
+            retailer_code,
+            product_number,
+            order_method_code,
+            to_char(date_::date, 'YYYY-Mon-DD') as ordered_date,
+            year(date_) as ordered_year,
+            concat('Q', '', quarter(date_)) as financialquarter,
+            quantity as quantity_sold,
+            selling_price,
+            cost_price,
+            round ((selling_price-cost_price)*quantity_sold, 0) AS profit
+        from {{ ref("stg_daily_sales") }}
+    ),
 
-    FROM {{ ref('stg_daily_sales') }}
-),
+    products AS (
+        SELECT * FROM {{ ref('stg_products') }}
+    ),
 
-retailers_daily_sales as (
-    SELECT *
-    FROM retailers
-    INNER JOIN
-    daily_sales
-    USING(RETAILER_CODE)
-)
-SELECT  * FROM retailers_daily_sales
+    order_type AS (
+        SELECT * FROM {{ ref('stg_order_methods') }}
+    ),
+
+
+
+    retailers_daily_sales as (
+        select * from retailers inner join daily_sales using (retailer_code)
+    ),
+
+    ret_das_pr AS (
+        select * 
+        from retailers_daily_sales
+        INNER JOIN
+        products
+        USING(PRODUCT_NUMBER)
+    ),
+
+    ret_das_pr_od AS (
+        SELECT * 
+        FROM ret_das_pr
+        INNER JOIN
+        order_type
+        USING(ORDER_METHOD_CODE)
+    )
+
+select * from ret_das_pr_od
